8e1a6926eef88e5a9e94d919bafec1974f1b16e1
Matthew Casey <mattcasey879@gmail.com>
2021-09-30 20:05:05 -0500

    finished all middlware

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -17,7 +17,7 @@ const { restricted, only } = require("../auth/auth-middleware.js");
     }
   ]
  */
-router.get("/", (req, res, next) => { // done for you
+router.get("/", restricted, (req, res, next) => { // done for you
   Users.find()
     .then(users => {
       res.json(users);
@@ -40,21 +40,12 @@ router.get("/", (req, res, next) => { // done for you
     }
   ]
  */
-router.get("/:user_id", (req, res, next) => { // done for you
+router.get("/:user_id", restricted, only('admin'), (req, res, next) => { // done for you
   Users.findById(req.params.user_id)
     .then(user => {
       res.json(user);
     })
     .catch(next);
 });
-
-router.post("/", (req, res, next) => {
-  const { username } = req.body
-  Users.findBy({ username })
-  .then(user => {
-    res.json(user);
-  })
-  .catch(next)
-})
 
 module.exports = router;
